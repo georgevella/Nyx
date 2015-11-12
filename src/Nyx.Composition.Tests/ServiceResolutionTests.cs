b013110cfc44c6c3fc0340b7ec94ebcc078c0a50
George Vella <admin@georgevella.com>
2015-11-12 18:05:01 +0100

    Moved TransientTestStub to seperate file

--- src/Nyx.Composition.Tests/ServiceResolutionTests.cs
+++ src/Nyx.Composition.Tests/ServiceResolutionTests.cs
@@ -186,21 +186,4 @@ namespace Nyx.Composition.Tests
 
         }
     }
-
-    public class TransientTestStub : IStub
-    {
-        public IStubDependency Dependency1 { get; set; }
-        public IStubDependency Dependency2 { get; set; }
-
-        public TransientTestStub(IStubDependency dependency1, IStubDependency dependency2)
-        {
-            Dependency1 = dependency1;
-            Dependency2 = dependency2;
-        }
-
-        public void Initializer()
-        {
-
-        }
-    }
 }
---
+++ src/Nyx.Composition.Tests/Stubs/TransientTestStub.cs
@@ -0,0 +1,19 @@
+namespace Nyx.Composition.Tests.Stubs
+{
+    public class TransientTestStub : IStub
+    {
+        public IStubDependency Dependency1 { get; set; }
+        public IStubDependency Dependency2 { get; set; }
+
+        public TransientTestStub(IStubDependency dependency1, IStubDependency dependency2)
+        {
+            Dependency1 = dependency1;
+            Dependency2 = dependency2;
+        }
+
+        public void Initializer()
+        {
+
+        }
+    }
+}
