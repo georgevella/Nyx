69295340a7c2f3a345497fce5e43de6b015a458e
George Vella <admin@georgevella.com>
2015-11-27 19:23:14 +0100

    Added about dialog to sample application

---
+++ samples/WpfSampleApplication/Commands/ShowAboutScreenCommand.cs
@@ -0,0 +1,26 @@
+﻿using Nyx.AppSupport.Commands;
+using Nyx.Presentation;
+using WpfSampleApplication.ViewModels;
+
+namespace WpfSampleApplication.Commands
+{
+    public class ShowAboutScreenCommand : BaseCommand
+    {
+        private readonly INavigator _navigator;
+
+        public ShowAboutScreenCommand(INavigator navigator)
+        {
+            _navigator = navigator;
+        }
+
+        public override void Execute()
+        {
+            _navigator.NavigateTo<AboutViewModel>();
+        }
+
+        public override bool CanExecute()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ samples/WpfSampleApplication/ViewModels/AboutViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Nyx.Presentation;
+using Nyx.Presentation.Attributes;
+
+namespace WpfSampleApplication.ViewModels
+{
+    [Dialog]
+    public class AboutViewModel : ViewModel<AboutViewModel>
+    {
+        public AboutViewModel(IUserInterfaceThread userInterfaceThread) : base(userInterfaceThread)
+        {
+        }
+    }
+}
