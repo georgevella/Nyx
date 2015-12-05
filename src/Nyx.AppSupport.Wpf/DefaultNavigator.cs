c7fe2440f445b25fff1fc1dc7e3dae3ddd09fb09
George Vella <admin@georgevella.com>
2015-12-05 14:49:24 +0100

    Navigator support for already viewmodels instantiated externally

--- src/Nyx.AppSupport.Wpf/DefaultNavigator.cs
+++ src/Nyx.AppSupport.Wpf/DefaultNavigator.cs
@@ -17,25 +17,26 @@ namespace Nyx.AppSupport
             _viewResolver = viewResolver;
         }
 
-        public TViewModel NavigateTo<TViewModel>() where TViewModel : IViewModel
+        public void NavigateTo<TViewModel>(TViewModel viewModel)
+            where TViewModel : IViewModel
         {
             var showModal = typeof(TViewModel).GetCustomAttribute<DialogAttribute>() != null;
-
-            return NavigateTo<TViewModel>(showModal);
+            NavigateTo(viewModel, showModal);
         }
 
-        public TViewModel NavigateTo<TViewModel>(bool showModal) where TViewModel : IViewModel
+        public void NavigateTo<TViewModel>(TViewModel viewModel, bool showModal)
+            where TViewModel : IViewModel
         {
             var viewType = _viewResolver.ResolveViewTypeFor<TViewModel>();
             var view = (FrameworkElement)_container.Get(viewType);
-            var viewModel = _container.Get<TViewModel>();
 
             view.DataContext = viewModel;
 
             var windowView = view as Window;
             if (windowView == null)
             {
-                return viewModel;
+                // TODO: the fuck we do when viewmodel is not a window?
+                return;
             }
 
             var mainWindow = Application.Current.MainWindow;
@@ -50,7 +51,19 @@ namespace Nyx.AppSupport
             {
                 windowView.Show();
             }
+        }
 
+        public TViewModel NavigateTo<TViewModel>() where TViewModel : IViewModel
+        {
+            var showModal = typeof(TViewModel).GetCustomAttribute<DialogAttribute>() != null;
+
+            return NavigateTo<TViewModel>(showModal);
+        }
+
+        public TViewModel NavigateTo<TViewModel>(bool showModal) where TViewModel : IViewModel
+        {
+            var viewModel = _container.Get<TViewModel>();
+            NavigateTo(viewModel, showModal);
             return viewModel;
         }
     }
--- src/Nyx.Framework/Presentation/INavigator.cs
+++ src/Nyx.Framework/Presentation/INavigator.cs
@@ -7,5 +7,11 @@
 
         TViewModel NavigateTo<TViewModel>(bool asDialog)
             where TViewModel : IViewModel;
+
+        void NavigateTo<TViewModel>(TViewModel viewModel)
+            where TViewModel : IViewModel;
+
+        void NavigateTo<TViewModel>(TViewModel viewModel, bool showModal)
+            where TViewModel : IViewModel;
     }
 }
